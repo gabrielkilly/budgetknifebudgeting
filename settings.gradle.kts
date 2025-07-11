9f3305731faf34468f68ef284ee8979a255ef010
Gabriel Killy <gabykilly@gmail.com>
2025-07-11 15:40:17 -0500

    first commit

---
+++ settings.gradle.kts
@@ -0,0 +1,21 @@
+rootProject.name = "ButterKnifeBudgeting"
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+include(":composeApp")
